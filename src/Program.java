303a69328cf7cefab655588ff4d51d5e3e61183d
Victor Santiago <victorhenriquedejesussantiago@gmail.com>
2024-04-01 09:02:16 -0300

    Manuseio das pecas capturadas

--- src/Program.java
+++ src/Program.java
@@ -2,7 +2,9 @@
 import chess.ChessMatch;
 import chess.ChessPiece;
 import chess.ChessPosition;
+import java.util.ArrayList;
 import java.util.InputMismatchException;
+import java.util.List;
 import java.util.Scanner;
 
 public class Program {
@@ -11,11 +13,13 @@ public class Program {
         //Board board = new Board(8, 8);
         Scanner sc = new Scanner(System.in);
         ChessMatch chessMatch = new ChessMatch();
-
+        List<ChessPiece> captured = new ArrayList<>();
+        
+        
         while (true) {
             try {
                 UI.clearScreen();
-                UI.printMatch(chessMatch);
+                UI.printMatch(chessMatch, captured);
                 System.out.println();
                 System.out.print("Procura: ");
                 ChessPosition source = UI.readChessPosition(sc);
@@ -28,6 +32,10 @@ public class Program {
                 ChessPosition target = UI.readChessPosition(sc);
 
                 ChessPiece capturedPiece = chessMatch.performChessMove(source, target);
+                
+                if(capturedPiece != null){
+                    captured.add(capturedPiece);
+                }
             } catch (InputMismatchException e) {
                 System.out.println(e.getMessage());
                 sc.nextLine();
