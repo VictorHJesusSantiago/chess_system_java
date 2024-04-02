5ffcfc2578b5775415ec13e670359e0077038c9c
Victor Santiago <victorhenriquedejesussantiago@gmail.com>
2024-04-02 07:03:06 -0300

    Movimento Especial EN_PASSANT

--- src/chess/pieces/Pawn.java
+++ src/chess/pieces/Pawn.java
@@ -1,86 +1,109 @@
-
 package chess.pieces;
 
 import boardgame.Board;
 import boardgame.Position;
+import chess.ChessMatch;
 import chess.ChessPiece;
 import chess.Color;
 
+public class Pawn extends ChessPiece {
 
-public class Pawn extends ChessPiece{
+    private ChessMatch chessMatch;
 
-    public Pawn(Board board, Color color) {
+    public Pawn(Board board, Color color, ChessMatch chessMatch) {
         super(board, color);
+        this.chessMatch = chessMatch;
     }
 
     @Override
     public boolean[][] possibleMoves() {
-        boolean[][] mat = new boolean[getBoard().getRows()][getBoard().getColumns()]; 
-        Position p = new Position(0,0);
-        
-        
-        if(getColor() == Color.WHITE){
-            
+        boolean[][] mat = new boolean[getBoard().getRows()][getBoard().getColumns()];
+        Position p = new Position(0, 0);
+
+        if (getColor() == Color.WHITE) {
+
             //UMA CASA A FRENTE PARA O PEÃO
             p.setValues(position.getRow() - 1, position.getColumn());
-            if(getBoard().positionExists(p) && !getBoard().thereIsaPiece(p)){
+            if (getBoard().positionExists(p) && !getBoard().thereIsaPiece(p)) {
                 mat[p.getRow()][p.getColumn()] = true;
             }
-            
+
             //DUAS CASAs A FRENTE PARA O PEÃO
             p.setValues(position.getRow() - 2, position.getColumn());
             Position p2 = new Position(position.getRow() - 1, position.getColumn());
-            if(getBoard().positionExists(p) && !getBoard().thereIsaPiece(p) && getBoard().positionExists(p2) && !getBoard().thereIsaPiece(p2) && getMoveCount() == 0){
+            if (getBoard().positionExists(p) && !getBoard().thereIsaPiece(p) && getBoard().positionExists(p2) && !getBoard().thereIsaPiece(p2) && getMoveCount() == 0) {
                 mat[p.getRow()][p.getColumn()] = true;
             }
-            
+
             //DIAGONAL ESQUERDA DO PEÃO
             p.setValues(position.getRow() - 1, position.getColumn() - 1);
-            if(getBoard().positionExists(p) && isThereOpponentPiece(p)){
+            if (getBoard().positionExists(p) && isThereOpponentPiece(p)) {
                 mat[p.getRow()][p.getColumn()] = true;
             }
-            
+
             //DIAGONAL DIREITA DO PEÃO
             p.setValues(position.getRow() - 1, position.getColumn() + 1);
-            if(getBoard().positionExists(p) && isThereOpponentPiece(p)){
+            if (getBoard().positionExists(p) && isThereOpponentPiece(p)) {
                 mat[p.getRow()][p.getColumn()] = true;
             }
-            
-        }
-        else{
+
+            //MOVIMENTO ESPECIAL EN PASSANT PARA OS BRANCOS
+            if (position.getRow() == 3) {
+                Position left = new Position(position.getRow(), position.getColumn() - 1);
+                if (getBoard().positionExists(left) && isThereOpponentPiece(left) && getBoard().piece(left) == chessMatch.getEnPassantVulnerable()) {
+                    mat[left.getRow() - 1][left.getColumn()] = true;
+                }
+                Position right = new Position(position.getRow(), position.getColumn() + 1);
+                if (getBoard().positionExists(right) && isThereOpponentPiece(right) && getBoard().piece(right) == chessMatch.getEnPassantVulnerable()) {
+                    mat[right.getRow() - 1][right.getColumn()] = true;
+                }
+            }
+        } else {
             //PEÃO PRETO
             //UMA CASA A FRENTE PARA O PEÃO
             p.setValues(position.getRow() + 1, position.getColumn());
-            if(getBoard().positionExists(p) && !getBoard().thereIsaPiece(p)){
+            if (getBoard().positionExists(p) && !getBoard().thereIsaPiece(p)) {
                 mat[p.getRow()][p.getColumn()] = true;
             }
-            
+
             //DUAS CASAs A FRENTE PARA O PEÃO
             p.setValues(position.getRow() + 2, position.getColumn());
             Position p2 = new Position(position.getRow() + 1, position.getColumn());
-            if(getBoard().positionExists(p) && !getBoard().thereIsaPiece(p) && getBoard().positionExists(p2) && !getBoard().thereIsaPiece(p2) && getMoveCount() == 0){
+            if (getBoard().positionExists(p) && !getBoard().thereIsaPiece(p) && getBoard().positionExists(p2) && !getBoard().thereIsaPiece(p2) && getMoveCount() == 0) {
                 mat[p.getRow()][p.getColumn()] = true;
             }
-            
+
             //DIAGONAL ESQUERDA DO PEÃO
             p.setValues(position.getRow() + 1, position.getColumn() - 1);
-            if(getBoard().positionExists(p) && isThereOpponentPiece(p)){
+            if (getBoard().positionExists(p) && isThereOpponentPiece(p)) {
                 mat[p.getRow()][p.getColumn()] = true;
             }
-            
+
             //DIAGONAL DIREITA DO PEÃO
             p.setValues(position.getRow() + 1, position.getColumn() + 1);
-            if(getBoard().positionExists(p) && isThereOpponentPiece(p)){
+            if (getBoard().positionExists(p) && isThereOpponentPiece(p)) {
                 mat[p.getRow()][p.getColumn()] = true;
             }
+
+            //MOVIMENTO ESPECIAL EN PASSANT PARA OS PRETOS
+            if (position.getRow() == 4) {
+                Position left = new Position(position.getRow(), position.getColumn() - 1);
+                if (getBoard().positionExists(left) && isThereOpponentPiece(left) && getBoard().piece(left) == chessMatch.getEnPassantVulnerable()) {
+                    mat[left.getRow() + 1][left.getColumn()] = true;
+                }
+                Position right = new Position(position.getRow(), position.getColumn() + 1);
+                if (getBoard().positionExists(right) && isThereOpponentPiece(right) && getBoard().piece(right) == chessMatch.getEnPassantVulnerable()) {
+                    mat[right.getRow() + 1][right.getColumn()] = true;
+                }
+            }
         }
-        
+
         return mat;
     }
-    
-    @Override 
-    public String toString (){
+
+    @Override
+    public String toString() {
         return "P";
     }
-    
+
 }
